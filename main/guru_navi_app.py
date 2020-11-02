3024ef90ed98f860acc0408ffc4a9fdca8fce41d
hashiguchi1544 <hashiguchi1544@gmail.com>
2020-11-02 16:34:11 +0900

    ぐるなびapiを使用方法を理解する

---
+++ main/guru_navi_app.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+import requests
+import os
+
+ACCESS_KEY = os.environ['ACCESS_KEY']
+
+
+def check_error(response):
+    try:
+        return response.json()['rest']
+    except KeyError:
+        return 'error'
+
+
+def get_input_word():
+    while True:
+        user_input = input('検索ワード ：')
+        url = f'https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid={ACCESS_KEY}&freeword={user_input}'
+        response = requests.get(url)
+        checked_error = check_error(response)
+
+        if checked_error == 'error':
+            print('無効な値です。再度入力していください m(__)m')
+            continue
+        else:
+            return checked_error
+
+
+def get_rest_name_link_lists(rest_list):
+    pprint(rest_list)
+    for rest_info in rest_list:
+        print(f"{rest_info['name']}  {rest_info['url']}")
+
+
+if __name__ == '__main__':
+    response = get_input_word()
+    get_rest_name_link_lists(response)
---
+++ tmp/環境変数.py
@@ -0,0 +1,8 @@
+import os
+from pprint import pprint
+
+# pprint(os.environ)
+print(os.environ['HOME'])
+print(os.environ['ACCESS_KEY'])
+print(os.environ['PYENV_SHELL'])
+print(os.environ['TEST_ACCESS_KEY'])
